a96d25d505db2d38e7da99180f808945b3a27475
Gilson Silva <gilsonsilvati@gmail.com>
2020-05-25 18:50:50 -0300

    first commit

---
+++ src/test/java/br/com/springmongodb/SpringMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
